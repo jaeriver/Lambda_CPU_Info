d728ed272106494b68b1b236597612b9b6a7951e
최재강 <chl8273@naver.com>
2022-01-19 16:57:05 +0900

    mistake;

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import subprocess
 import json
 
-lambda_handler(event, context):
+def lambda_handler(event, context):
   output = subprocess.check_output(["cat", "/proc/cpuinfo"], shell=True, encoding='utf-8')
   print(output)
   return output
